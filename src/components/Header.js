69a9451feb82df50ab773f3a0e6b816a42e23fef
Douglas Obara <douglasobara97@gmail.com>
2022-07-30 05:47:16 +0300

    add contact button

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,11 @@ const Header = () => {
         <div className="logo col-12 col-md-3" title="Cuisine Dishes">
           🍔 Cuisine Dishes
         </div>
-        <div className="midTitle col-12 col-md-6">Meal Cuisine Restaurant</div>
-        <div className="contactBtn col-12 col-md-3">
-          <Nav.Link href="/contact">Contact Us</Nav.Link>
+        <div className="midTitle col-12 col-md-7">Meal Cuisine Restaurant</div>
+        <div className="contactBtn col-12 col-md-2">
+          <Nav.Link className="contact_btn_link" href="#">
+            Contact Us
+          </Nav.Link>
         </div>
       </div>
     </>
